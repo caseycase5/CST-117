a56b24f38d5cf96fb87c76d23a26e7249b4fec4a
caseycase5 <caseycase5@gmail.com>
2020-10-17 18:16:53 +0000

    Exercise 1

---
+++ Exercise1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Exercise1
+{
+	partial class PictureView
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(334, 385);
+			this.button1.MaximumSize = new System.Drawing.Size(120, 25);
+			this.button1.MinimumSize = new System.Drawing.Size(100, 20);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(107, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "View Picture";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Image = global::Exercise1.Properties.Resources.Test_pic;
+			this.pictureBox1.Location = new System.Drawing.Point(120, 53);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(564, 298);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.progressBar1.Location = new System.Drawing.Point(334, 414);
+			this.progressBar1.MaximumSize = new System.Drawing.Size(200, 25);
+			this.progressBar1.MinimumSize = new System.Drawing.Size(200, 25);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(200, 25);
+			this.progressBar1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(120, 34);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Picture 1";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// PictureView
+			// 
+			this.AcceptButton = this.button1;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "PictureView";
+			this.Text = "Picture View";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
