d05ac715b497e6a60a141648cdb4f1128b63ac1e
caseycase5 <caseycase5@gmail.com>
2020-10-19 00:00:42 +0000

    Add files via upload

---
+++ Exercise3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Exercise3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.inchesText = new System.Windows.Forms.TextBox();
+			this.cmText = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// inchesText
+			// 
+			this.inchesText.Location = new System.Drawing.Point(172, 108);
+			this.inchesText.Name = "inchesText";
+			this.inchesText.Size = new System.Drawing.Size(100, 20);
+			this.inchesText.TabIndex = 0;
+			// 
+			// cmText
+			// 
+			this.cmText.Location = new System.Drawing.Point(172, 151);
+			this.cmText.Name = "cmText";
+			this.cmText.Size = new System.Drawing.Size(100, 20);
+			this.cmText.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(82, 115);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(70, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Enter Inches:";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(28, 158);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(124, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Inches converted to CM:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(197, 201);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Convert";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(356, 299);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cmText);
+			this.Controls.Add(this.inchesText);
+			this.Name = "Form1";
+			this.Text = "In to Cm Conversion";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox inchesText;
+		private System.Windows.Forms.TextBox cmText;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Exercise3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3
+{
+	public partial class Form1 : Form
+	{
+		public String inchText;
+		public double inch;
+		public double cm;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			inchText = inchesText.Text;
+			inch = Convert.ToDouble(inchText);
+			cm = inch * 2.54;
+			cmText.Text = Convert.ToString(cm);
+		}
+	}
+}
