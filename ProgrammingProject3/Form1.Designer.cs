10b83338c6fb54db0c40ff86dc6e20fca525dc28
caseycase5 <caseycase5@gmail.com>
2020-11-01 20:51:01 +0000

    Programming Project 3

---
+++ ProgrammingProject3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProgrammingProject3
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.finalText = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File Name:";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.userInput.Location = new System.Drawing.Point(145, 12);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(275, 23);
+            this.userInput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // finalText
+            // 
+            this.finalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.finalText.FormattingEnabled = true;
+            this.finalText.ItemHeight = 16;
+            this.finalText.Location = new System.Drawing.Point(17, 147);
+            this.finalText.Name = "finalText";
+            this.finalText.Size = new System.Drawing.Size(555, 292);
+            this.finalText.TabIndex = 3;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 455);
+            this.Controls.Add(this.finalText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form";
+            this.Text = "Read/Write Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox finalText;
+    }
+}
+
---
+++ ProgrammingProject3/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingProject3
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String[] lines;
+            String[] linesCopy = null;
+            String userFile;
+            int arraySize;
+            int longestWord = 0;
+            int longestIndex = 0;
+            int wordLength;
+            int noVowelsLength;
+            int mostVowels = 0;
+            int mostVowelsIndex = 0;
+
+            // Reading the file
+            lines = System.IO.File.ReadAllLines(userInput.Text);
+            finalText.Items.Add("Initial list of words read from file: ");
+            finalText.Items.Add("");
+            finalText.Items.AddRange(lines);
+            finalText.Items.Add("");
+            arraySize = lines.Length - 1;
+
+            // Converting to Lowercase
+            for (int x = 0; x < lines.Length; x++)
+            {
+                lines[x] = lines[x].ToLower();
+            }
+
+            finalText.Items.Add("Converting all to lowercase: ");
+            finalText.Items.Add("");
+            finalText.Items.AddRange(lines);
+            finalText.Items.Add("");
+
+            // Sorting the list alphabetically and printing first/last words
+            Array.Sort(lines);
+            finalText.Items.Add("The first word alphabetically is: ");
+            finalText.Items.Add(lines[0]);
+            finalText.Items.Add("");
+            finalText.Items.Add("The last word alphabetically is: ");
+            finalText.Items.Add(lines[arraySize]);
+            finalText.Items.Add("");
+
+            // Finding the longest word
+            for(int x = 0; x < lines.Length; x++)
+            {
+                if(lines[x].Length > longestWord)
+                {
+                    longestWord = lines[x].Length;
+                    longestIndex = x;
+                }
+            }
+
+            finalText.Items.Add("The longest word is: ");
+            finalText.Items.Add(lines[longestIndex]);
+            finalText.Items.Add("");
+
+            // Finding the word with the most vowels
+            linesCopy = (String[])lines.Clone();
+            for (int x = 0; x < lines.Length; x++)
+            {
+                wordLength = lines[x].Length;
+
+                if(linesCopy[x].Contains("a"))
+                {
+                    linesCopy[x] = linesCopy[x].Replace("a", "");
+                }
+                if (linesCopy[x].Contains("e"))
+                {
+                    linesCopy[x] = linesCopy[x].Replace("e", "");
+                }
+                if (linesCopy[x].Contains("i"))
+                {
+                    linesCopy[x] = linesCopy[x].Replace("i", "");
+                }
+                if (linesCopy[x].Contains("o"))
+                {
+                    linesCopy[x] = linesCopy[x].Replace("o", "");
+                }
+                if (linesCopy[x].Contains("u"))
+                {
+                    linesCopy[x] = linesCopy[x].Replace("u", "");
+                }
+                noVowelsLength = linesCopy[x].Length;
+
+                if(noVowelsLength > mostVowels)
+                {
+                    mostVowels = wordLength - noVowelsLength;
+                    mostVowelsIndex = x;
+                }
+            }
+
+            finalText.Items.Add("The first word with the most vowels is: ");
+            finalText.Items.Add("");
+            finalText.Items.Add(lines[mostVowelsIndex] + " with " + mostVowels + " vowels.");
+
+            // Saving to a file
+            const string sPath = "SaveFile.txt";
+
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in finalText.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+
+            MessageBox.Show("Program is Saved!");
+        }
+    }
+}
