de064b9c9c1159b877ae47d524fdb201e00cfcf6
caseycase5 <caseycase5@gmail.com>
2020-11-15 18:12:12 +0000

    Exercise 8 for CST-117

---
+++ Exercise 8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+		public static void Main()
+		{
+			int fatCalories;
+			int carbCalories;
+
+			fatCalories = FatCalories(14);
+			carbCalories = CarbCalories(22);
+			Console.WriteLine("The number of calories from fat are " + fatCalories);
+			Console.WriteLine("The number of calories from carbs are " + carbCalories);
+
+		}
+
+		public static int FatCalories(int fatGrams)
+		{
+			return (fatGrams * 9);
+		}
+
+		public static int CarbCalories(int carbGrams)
+		{
+			return (carbGrams * 4);
+		}
+	}
+}
